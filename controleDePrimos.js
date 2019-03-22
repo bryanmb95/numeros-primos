c34c6648d2d86d825a365fc6beca78dc2d643645
bryanmb95 <39245387+bryanmb95@users.noreply.github.com>
2019-03-21 22:47:20 -0300

    Add files via upload
    Adicionado função de teste se um numero é primo ou não

--- controleDePrimos.js
+++ controleDePrimos.js
@@ -5,8 +5,19 @@ function testarSoma(num1, num2) {
 }
 
 function testarPrimo(num1) {
-    var antecessor = num1 - 1;
+    var contador = 0;
+    var x;
+    for (x = 1; x <= num1; x++) { //Verifica as divisibilidades do numero inserio
+        if (num1 % x == 0) {
+            contador++;
+        }
+    }
 
+    if (contador == 2) {
+        return "O numero eh primo";
+    } else {
+        return "O numero nao eh primo";
+    }
 }
 
 module.exports = {
